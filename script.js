c4d16bc554de651a99335a10f3a7b3a9ac80ab4a
mohiuddinmohi <mohiuddin0240@gmail.com>
2021-04-26 23:30:56 +0600

    js functionality added

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const btn = document.querySelector('.btn')
-const search = document.querySelector('.search')
-const input = document.querySelector('.input')
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
 
-btn.addEventListener('click', () => {
-  search.classList.toggle('active')
-  input.focus()
-})
+let load = 0
+
+const int = setInterval(blurring, 30)
+
+function blurring() {
+  load ++
+  if (load > 99) {
+    clearInterval(int)
+  }
+
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+function scale (number, inMin, inMax, outMin, outMax) {
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
